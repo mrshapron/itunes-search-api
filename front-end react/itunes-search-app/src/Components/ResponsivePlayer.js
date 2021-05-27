0000bf6cd2f24b4e22e12665e9f179a162fbb9c3
Sharon Pais <mrshapron@gmail.com>
2021-05-27 13:20:20 +0300

    css changes
    fixing centerize the audio player (look better on phones dimensions)

--- front-end react/itunes-search-app/src/Components/ResponsivePlayer.js
+++ front-end react/itunes-search-app/src/Components/ResponsivePlayer.js
@@ -6,14 +6,12 @@ const ResponsivePlayer = ({ url }) => {
   return (
     <div>
       <link rel="stylesheet" href="react-h5-audio-player/lib/styles.less" />
-      <div className="audio-player">
         <AudioPlayer
           width="300px"
           autoPlay
           src={url}
           onPlay={e => console.log("playing")}
         />
-      </div>
     </div>
   );
 };
